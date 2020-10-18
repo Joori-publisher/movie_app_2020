93acbec0b3d28876a2bdf08e88f42dd7465f85b3
joori-fe <68975884+joori-fe@users.noreply.github.com>
2020-10-18 09:53:19 +0900

    #3.0 Class Components and State (10:11)

--- src/App.js
+++ src/App.js
@@ -1,44 +1,25 @@
 import React from 'react';
 
-const thumb=[
-  {
-    id:1,
-    name:'(FULL) [연애혁명] EP.1 공주와 왕자, 우린 운명(?)이야 [2020.09.01]',
-    image:'https://t1.daumcdn.net/thumb/C240x140.fjpg.q75/?fname=https%3A%2F%2Ft1.kakaocdn.net%2Fkakaotv%2FASSET%2Fwarhol_asset%2Fvod_thumbnail%2F464536ccd46767a6496f1b294959232d809f3eb0'
-  },
-  {
-    id:2,
-    name:'(FULL) [연애혁명] EP.2 사랑은... 타이밍이지?! [2020.09.03]',
-    image:'https://t1.daumcdn.net/thumb/C240x140.fjpg.q75/?fname=https%3A%2F%2Ft1.kakaocdn.net%2Fkakaotv%2FASSET%2Fwarhol_asset%2Fvod_thumbnail%2F0ff824072a0c5c787543893760f5662d84735d99'
-  },
-  {
-    id:3,
-    name:'(FULL) [연애혁명] EP.3 그날 수련회에서 생긴 일 [2020.09.10]',
-    image:'https://t1.daumcdn.net/thumb/C240x140.fjpg.q75/?fname=https%3A%2F%2Ft1.kakaocdn.net%2Fkakaotv%2FASSET%2Fwarhol_asset%2Fvod_thumbnail%2Fee4cb5fe8ba9b65f2b163782851ac0cd8af5da85'
-  },
-  {
-    id:4,
-    name:'(FULL) [연애혁명] EP.4 수련회 고백(GO BACK) 사건 [2020.09.17]',
-    image:'https://t1.daumcdn.net/thumb/C240x140.fjpg.q75/?fname=https%3A%2F%2Ft1.kakaocdn.net%2Fkakaotv%2FASSET%2Fwarhol_asset%2Fvod_thumbnail%2F062c73f2348501184ff17ac4945b0ec1aec70e19'
-  }
-]
+class App extends React.Component{
+  
+  state={
+    count:0
+  };
+  add=()=>{
+    console.log("add");
+  };
+  minus=()=>{
+    console.log("minus");
+  };
 
-function List({name,image}){
+  render(){
   return (
     <div>
-      <p>{name}</p>
-      <img src={image} alt={name}/>
+      <h1>The number is {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-  )
-}
-
-
-function App() {
-  return (
-    <div>
-      { thumb.map(list => <List key={list.id} name={list.name} image={list.image}/>) }
-    </div>
-  );
+  )}
 }
 
 export default App;
